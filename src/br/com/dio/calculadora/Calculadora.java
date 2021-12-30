c7e8a0e663f5daa08eac27561aa1da81c7ca7076
danielgomees <danielgomesdeoliveira@outlook.com.br>
2021-12-30 00:10:08 -0300

    Atualização da exibição dos resultados com casas decimais.

--- src/br/com/dio/calculadora/Calculadora.java
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -23,10 +23,10 @@ public class Calculadora {
 		double multiplicacao = multiplicacao(a,b);
 		double divisao = divisao(a,b);
 		
-		System.out.println("Soma: " + soma);
-		System.out.println("Subtracao: " + subtracao);
-		System.out.println("Multiplicacao: " + multiplicacao);
-		System.out.println("Divisao: " + String.format("%.2f", divisao));
+        System.out.println("soma: " + String.format("%.0f", soma));
+        System.out.println("subtração: " + String.format("%.0f", subtracao));
+        System.out.println("multiplição: " + String.format("%.0f", multiplicacao));
+        System.out.println("divisão: " + String.format("%.1f", divisao));
 		
 	}
 
